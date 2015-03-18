cccd0ec219d90c9e8e74fccb370ed884424b8859
Tuomas Jokioja <jokioja92@hotmail.com>
2015-03-18 02:58:55 +0200

    LoginActivity, MainActivity, VarausActivity Tehty primitiivinen login-systeemi MainActivity spinner ja button -> VarausActivity

--- app/src/main/java/com/example/antti/androidvaraus/MainActivity.java
+++ app/src/main/java/com/example/antti/androidvaraus/MainActivity.java
@@ -1,17 +1,66 @@
 package com.example.antti.androidvaraus;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public final static String EXTRA_MESSAGE2 = "com.example.antti.androidvaraus.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String nimi = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE1);
+        String[] osat = nimi.split("@");
+        TextView textView = (TextView) findViewById(R.id.tervetuloa);
+        textView.setText("Hei " + osat[0] + "!");
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.esitykset);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.elokuva_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Button logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin(v);
+            }
+        }
+        );
+
+        Button varaa = (Button) findViewById(R.id.varaus_button);
+        varaa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openVaraus(v);
+            }
+        });
+    }
+
+    private void openLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void openVaraus(View view){
+        Intent intent = new Intent(this, VarausActivity.class);
+        Spinner spinner = (Spinner) findViewById(R.id.esitykset);
+        String message = spinner.getSelectedItem().toString();
+        intent.putExtra(EXTRA_MESSAGE2, message);
+        startActivity(intent);
     }
 
 
