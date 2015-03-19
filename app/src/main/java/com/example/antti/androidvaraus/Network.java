b61f45adeb1a1289896da79c3acfd1ed8c5ee111
Aleksi Blinnikka <aleksi.blinnikka@gmail.com>
2015-03-20 01:45:23 +0200

    Download/upload toteutettu

---
+++ app/src/main/java/com/example/antti/androidvaraus/Network.java
@@ -0,0 +1,99 @@
+package com.example.antti.androidvaraus;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Handles networking requests.
+ */
+public class Network {
+    private static final String UPLOAD_URL = "http://woodcomb.aleksib.fi/up";
+
+    private static final String POST_START =
+        "-----------------------------175223208916369\r\n" +
+        "Content-Disposition: form-data; name=\"file\"; filename=\"";
+    private static final String POST_MIDDLE = "\"\r\nContent-Type: text/plain\r\n\r\n";
+    private static final String POST_END = "\r\n-----------------------------175223208916369--\r\n\r\n";
+
+    public static String download(URL url) {
+        StringBuilder buffer = new StringBuilder(512);
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+                for (;;) {
+                    int ret = inputStream.read();
+                    if (ret != -1) {
+                        buffer.append((char)ret);
+                    } else {
+                        inputStream.close();
+                        break;
+                    }
+                }
+            } finally {
+                connection.disconnect();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return buffer.toString();
+    }
+
+    public static boolean upload(String data, String filename) {
+        boolean status = false;
+        StringBuilder buffer = new StringBuilder(256);
+        buffer.append(POST_START);
+        buffer.append(filename);
+        buffer.append(POST_MIDDLE);
+        buffer.append(data);
+        buffer.append(POST_END);
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection)(new URL(UPLOAD_URL)).openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setUseCaches(false);
+            connection.setFixedLengthStreamingMode(buffer.length());
+            connection.setRequestProperty(
+                    "Content-Type",
+                    "multipart/form-data; boundary=---------------------------175223208916369");
+
+            try {
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+                writer.write(buffer.toString());
+                writer.flush();
+                writer.close();
+
+                buffer = new StringBuilder();
+                InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
+                for (;;) {
+                    int ret = inputStream.read();
+                    if (ret != -1) {
+                        buffer.append((char)ret);
+                    } else {
+                        inputStream.close();
+                        break;
+                    }
+                }
+
+                if (buffer.toString().contains("1")) {
+                    status = true;
+                }
+            } finally {
+                connection.disconnect();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return status;
+    }
+}
