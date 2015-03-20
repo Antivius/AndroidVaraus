c3d8915d8358f99715af7382f48f8552c9f2b0a3
Aleksi Blinnikka <aleksi.blinnikka@gmail.com>
2015-03-20 16:34:50 +0200

    VarausActivity hakee tiedot verkosta

--- app/src/main/java/com/example/antti/androidvaraus/VarausActivity.java
+++ app/src/main/java/com/example/antti/androidvaraus/VarausActivity.java
@@ -1,9 +1,10 @@
 package com.example.antti.androidvaraus;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,17 +13,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class VarausActivity extends ActionBarActivity {
 
     public final static String EXTRA_MESSAGE3 = "com.example.antti.androidvaraus.MESSAGE3";
+    private static final String NAYTOS_URL = "http://woodcomb.aleksib.fi/files/naytokset.txt";
     private String elokuva;
     private String kayttaja;
+    private Map<String, String> naytokset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,62 +43,63 @@ public class VarausActivity extends ActionBarActivity {
             naytos.setText("Elokuvan " + elokuva + " näytökset");
         }
 
-        ArrayList<String> listViewarrayList = haeNaytoksetLista();
-
         final ListView naytoslista = (ListView) findViewById(R.id.naytokset);
-        ArrayAdapter<String> listadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listViewarrayList);
+        ArrayAdapter<String> listadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         naytoslista.setAdapter(listadapter);
+        try {
+            // Start a background task to download the available movies
+            new NaytosTask().execute(new Pair<>(new URL(NAYTOS_URL), listadapter));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         naytoslista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 nappulaPainettu.setText("Nappula painettu!");
                 Object listItem = naytoslista.getItemAtPosition(position);
                 String s = listItem.toString();
-                String[] slista = s.split(" ");
-                String viesti = slista[slista.length -1] + ":" + kayttaja;
+                String viesti = kayttaja + ":" + naytokset.get(s);
 
                 Intent intent = new Intent(getApplicationContext(), PaikkaActivity.class);
                 intent.putExtra(EXTRA_MESSAGE3, viesti);
                 startActivity(intent);
                 finish();
-
-
             }
         });
     }
 
-    private ArrayList<String> haeNaytoksetLista(){
-        ArrayList<String> lista = new ArrayList<String>();
-        String elokuva = this.elokuva;
-        try {
+    private class NaytosTask extends AsyncTask<Pair<URL, ArrayAdapter<String>>, Void, ArrayAdapter<String>> {
+        protected ArrayAdapter<String> doInBackground(Pair<URL, ArrayAdapter<String>>... pairs) {
+            naytokset = new HashMap<>();
 
-            AssetManager am = getAssets();
-            BufferedReader in1 = null;
+            if (pairs.length != 1) {
+                return null;
+            }
 
-            in1 = new BufferedReader(new InputStreamReader(am.open("naytokset.txt")));
-
-            String line1;
-            while((line1 = in1.readLine()) != null){
-                String viesti1 = "";
-                String[] osat1;
-                osat1 = line1.split(":");
-                if(elokuva.equals("Kaikki")){
-                    viesti1 = osat1[3] + " " + osat1[4] + " " + osat1[5] + " " + osat1[1] + " " + osat1[2] + " id: " + osat1[0] ;
-                    lista.add(viesti1);
-                }else {
-                    if (osat1[5].equals(elokuva)) {
-                        viesti1 = osat1[3] + " " + osat1[4] + " " + osat1[5] + " " + osat1[1] + " " + osat1[2] + " id: " + osat1[0] ;
-                        lista.add(viesti1);
-
+            String lines = Network.download(pairs[0].first);
+            for (String line : lines.split("\n")) {
+                //formaatti: ID:Teatteri:Sali:Pvm:Klo:Nimi
+                //001:Teatteri1:Sali1:18.03.2015:18.00:Interstellar
+                String[] osat = line.split(":", 6);
+                if (elokuva.equals("Kaikki")) {
+                    String naytos = osat[3] + " " + osat[4] + " " + osat[5] + " " + osat[1] + " " + osat[2];
+                    naytokset.put(naytos, line);
+                } else {
+                    if (osat[5].equals(elokuva)) {
+                        String naytos = osat[3] + " " + osat[4] + " " + osat[5] + " " + osat[1] + " " + osat[2];
+                        naytokset.put(naytos, line);
                     }
                 }
             }
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lista;
-    }
 
+            return pairs[0].second;
+        }
+
+        protected void onPostExecute(ArrayAdapter<String> adapter) {
+            adapter.addAll(naytokset.keySet());
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
