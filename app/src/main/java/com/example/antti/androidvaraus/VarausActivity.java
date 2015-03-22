a37de3f49019ee2508c1a856a9626171e9668663
Aleksi Blinnikka <aleksi.blinnikka@gmail.com>
2015-03-22 16:52:39 +0200

    Näytä vain tulevat näytökset, poista vanhat

--- app/src/main/java/com/example/antti/androidvaraus/VarausActivity.java
+++ app/src/main/java/com/example/antti/androidvaraus/VarausActivity.java
@@ -13,6 +13,9 @@ import android.widget.TextView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,10 +66,13 @@ public class VarausActivity extends ActionBarActivity {
 
     private class NaytosTask extends AsyncTask<Pair<URL, ArrayAdapter<String>>, Void, Void> {
         private ArrayAdapter<String> adapter;
+        private String showsFile;
+        private ArrayList<String> showsToRemove;
 
         @SafeVarargs
         protected final Void doInBackground(Pair<URL, ArrayAdapter<String>>... pairs) {
             shows = new HashMap<>();
+            showsToRemove = new ArrayList<>();
 
             if (pairs.length != 1) {
                 return null;
@@ -74,8 +80,11 @@ public class VarausActivity extends ActionBarActivity {
 
             adapter = pairs[0].second;
 
-            String lines = Network.download(pairs[0].first);
-            for (String line : lines.split("\n")) {
+            showsFile = Network.download(pairs[0].first);
+            for (String line : showsFile.split("\n")) {
+                if (line.isEmpty()) {
+                    continue;
+                }
                 //formaatti: ID:Teatteri:Sali:Pvm:Klo:Nimi
                 //001:Teatteri1:Sali1:18.03.2015:18.00:Interstellar
                 String[] parts = line.split(":", 6);
@@ -84,13 +93,69 @@ public class VarausActivity extends ActionBarActivity {
                     shows.put(show, line);
                 } else {
                     if (parts[5].equals(movie)) {
-                        String show = parts[3] + " " + parts[4] + " " + parts[1] + " " + parts[2];
-                        shows.put(show, line);
+                        // Poista kaikki jo menneet näytökset
+                        Date currentTime = new Date();
+                        int date = Integer.parseInt(parts[3].substring(0, 2));
+                        int month = Integer.parseInt(parts[3].substring(3, 5));
+                        int year = Integer.parseInt(parts[3].substring(6));
+                        int hrs = Integer.parseInt(parts[4].substring(0, 2));
+                        int min = Integer.parseInt(parts[4].substring(3));
+                        Calendar c = Calendar.getInstance(); c.set(year, month - 1, date, hrs, min);
+                        Date showTime = c.getTime();
+                        if (currentTime.compareTo(showTime) <= 0) {
+                            String show = parts[3] + " " + parts[4] + " " + parts[1] + " " + parts[2];
+                            shows.put(show, line);
+                        } else {
+                            showsToRemove.add(line);
+                        }
                     }
                 }
             }
 
+            removeShows();
+
             return null;
+        }
+
+        private void removeShows() {
+            if (showsToRemove.size() == 0) {
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder(showsFile.length());
+
+            try {
+                String reservations = Network.download(new URL(Network.RESERV_URL));
+                ArrayList<String> removedShowIds = new ArrayList<>();
+
+                for (String line : showsFile.split("\n")) {
+                    if (!showsToRemove.contains(line) && !line.isEmpty()) {
+                        sb.append(line);
+                        sb.append("\n");
+                    } else {
+                        removedShowIds.add(line.split(":", 2)[0]);
+                    }
+                }
+
+                showsFile = sb.toString();
+                Network.upload(showsFile, "naytokset.txt");
+                sb = new StringBuilder(reservations.length());
+
+                for (String line : reservations.split("\n")) {
+                    if (line.isEmpty()) {
+                        continue;
+                    }
+                    String reservationShowId = line.split(":", 2)[0];
+                    if (!removedShowIds.contains(reservationShowId)) {
+                        sb.append(line);
+                        sb.append("\n");
+                    }
+                }
+
+                Network.upload(sb.toString(), "varaukset.txt");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
 
         protected void onPostExecute(Void _) {
