6cd21b9bdd9fa9e78a91a9440685f329d0391ad6
Aleksi Blinnikka <aleksi.blinnikka@gmail.com>
2015-03-22 12:52:20 +0200

    Siivoa AdminActivity

--- app/src/main/java/com/example/antti/androidvaraus/AdminActivity.java
+++ app/src/main/java/com/example/antti/androidvaraus/AdminActivity.java
@@ -1,20 +1,13 @@
 package com.example.antti.androidvaraus;
 
 import android.content.Intent;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.io.File;
 
 
 public class AdminActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,104 +17,53 @@ public class AdminActivity extends ActionBarActivity {
         aButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                manageMovies(findViewById(R.id.manageMoviesButton));
+                manageMovies();
             }
         });
 
-        Button manageUsersbutton = (Button) findViewById(R.id.manageUsersButton);
-        manageUsersbutton.setOnClickListener(new View.OnClickListener() {
+        Button manageUsersButton = (Button) findViewById(R.id.manageUsersButton);
+        manageUsersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                manageUsers(findViewById(R.id.manageMoviesButton));
+                manageUsers();
             }
         });
 
-        final Button manageShowbutton = (Button) findViewById(R.id.manageShowButton);
-        manageShowbutton.setOnClickListener(new View.OnClickListener() {
+        final Button manageShowButton = (Button) findViewById(R.id.manageShowButton);
+        manageShowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                manageShow(findViewById(R.id.manageShowButton));
+                manageShow();
             }
         });
 
-        Button aLogout = (Button) findViewById(R.id.aLogout);
-        aLogout.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          openLogin(v);
-                                      }
-                                  }
-        );
-
-
+        Button logoutButton = (Button) findViewById(R.id.aLogout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
     }
 
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_admin, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
-
-    public  void manageMovies(View view){
-        Intent intent = new Intent(this,AddMovieActivity.class);
+    private void manageMovies() {
+        Intent intent = new Intent(this, AddMovieActivity.class);
         startActivity(intent);
     }
 
-    public  void manageUsers(View view){
-        Intent intent = new Intent(this,AddUserActivity.class);
+    private void manageUsers() {
+        Intent intent = new Intent(this, AddUserActivity.class);
         startActivity(intent);
     }
 
-    public void manageShow(View view){
-        Intent intent = new Intent(this,AddShowActivity.class);
+    private void manageShow() {
+        Intent intent = new Intent(this, AddShowActivity.class);
         startActivity(intent);
     }
 
-
-    /**
-     * Kirjoitustesti, ei taida toimia
-     */
-
-    public void writeStuff(){
-        TextView tv = (TextView) findViewById(R.id.adminText);
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root + "/folder");
-        myDir.mkdirs();
-        String fname = "testFile";
-        File file = new File (myDir, fname);
-        tv.setText("done");
-    }
-
-    private void openLogin(View view){
+    private void openLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
-
-
-
-
-
 }
